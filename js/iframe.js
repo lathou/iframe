4de93554a875f64061d870d275d2a0eb6488dfdb
lathou <tholfou@gmail.com>
2021-05-26 15:04:59 +0200

    add javascript to play video

--- js/iframe.js
+++ js/iframe.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
+const playBtn = document.querySelector('.js-play');
+const wrapper = document.querySelector('.wrapper');
 
-});
+playBtn.addEventListener('click', () => {
+    console.log('play');
+    wrapper.innerHTML = "<iframe frameborder=\"0\" allowfullscreen=\"1\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" title=\"YouTube video player\" width=\"50%\" height=\"100%\" src=\"https://www.youtube.com/embed/Bsamw_hpL4A?autoplay=1&mute=0&controls=1&origin=https%3A%2F%2Frecette.devoirs.objectif-crpe.fr&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&enablejsapi=1&widgetid=1\" id=\"widget2\"></iframe>"
+})
