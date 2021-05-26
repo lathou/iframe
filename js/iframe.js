49a900066bafe3a7206957688d360ab284be5648
lathou <tholfou@gmail.com>
2021-05-26 17:42:54 +0200

    load api only once

--- js/iframe.js
+++ js/iframe.js
@@ -7,14 +7,21 @@ playBtn.addEventListener('click', () => {
     wrapper.innerHTML = "<iframe frameborder=\"0\" allowfullscreen=\"1\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" title=\"YouTube video player\" width=\"50%\" height=\"100%\" src=\"https://www.youtube.com/embed/Bsamw_hpL4A?autoplay=1&mute=0&controls=1&origin=https%3A%2F%2Frecette.devoirs.objectif-crpe.fr&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&enablejsapi=1&widgetid=1\" id=\"widget2\"></iframe>"
 });
 
-playBtnYt.addEventListener('click', () => loadApi('youtube'));
+playBtnYt.addEventListener('click', () => {
+    loadApi('youtube', '8l5yhh0m1HA')
+        .then(createYoutubeIframe);
+});
 
-function onYouTubeIframeAPIReady() {
-    //TODO: load the targeted video only
+playBtnVimeo.addEventListener('click', () => {
+    loadApi('vimeo', 'some vimeo id')
+        .then(createVimeoIframe);
+});
+
+function createYoutubeIframe(videoId) {
     const player = new YT.Player('yt-player', {
         height: '390',
         width: '640',
-        videoId: '8l5yhh0m1HA',
+        videoId: videoId,
         playerVars: {
             'playsinline': 1
         },
@@ -24,22 +31,39 @@ function onYouTubeIframeAPIReady() {
     });
 }
 
-playBtnVimeo.addEventListener('click', () => {
-    loadApi('vimeo');
-    //TODO: Wait for the script to be ready (and remove setTimeout)
-    setTimeout(createVimeoIframe, 1000)
-});
-
-function createVimeoIframe() {
+function createVimeoIframe(videoId) {
     const player = new Vimeo.Player('vimeo-player');
     player.play()
 }
 
-function loadApi(provider) {
-    //TODO: Only if script has not yet been inserted
-    var tag = document.createElement('script');
+let youtubeResolve;
 
-    tag.src = provider === "youtube" ? "https://www.youtube.com/iframe_api" : "https://player.vimeo.com/api/player.js";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+function onYouTubeIframeAPIReady() {
+    youtubeResolve()
+}
+
+function loadApi(provider, videoId) {
+    const isYoutubeIframe = provider === "youtube";
+
+    return new Promise(function (resolve, reject) {
+
+        const scriptId = isYoutubeIframe ? "yt-iframe-api" : "vimeo-iframe-api";
+        if(document.querySelector('#' + scriptId)) resolve(videoId);
+
+        const tag = document.createElement('script');
+        tag.id = scriptId;
+
+        if(isYoutubeIframe) {
+            tag.src = "https://www.youtube.com/iframe_api";
+            if (isYoutubeIframe) {
+                youtubeResolve = () => resolve(videoId);
+            }
+        } else {
+            tag.src = "https://player.vimeo.com/api/player.js";
+            tag.addEventListener('load', () => resolve(videoId)) ;
+        }
+
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    });
 }
