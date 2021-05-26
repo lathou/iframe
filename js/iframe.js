e55ad8e0a05469281c9b24104155652ac47842a4
lathou <tholfou@gmail.com>
2021-05-26 15:41:49 +0200

    fix yt api

--- js/iframe.js
+++ js/iframe.js
@@ -7,27 +7,28 @@ playBtn.addEventListener('click', () => {
     wrapper.innerHTML = "<iframe frameborder=\"0\" allowfullscreen=\"1\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" title=\"YouTube video player\" width=\"50%\" height=\"100%\" src=\"https://www.youtube.com/embed/Bsamw_hpL4A?autoplay=1&mute=0&controls=1&origin=https%3A%2F%2Frecette.devoirs.objectif-crpe.fr&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&enablejsapi=1&widgetid=1\" id=\"widget2\"></iframe>"
 });
 
-playBtnYt.addEventListener('click', () => {
+playBtnYt.addEventListener('click', loadIframe);
+
+function onYouTubeIframeAPIReady() {
+    var player;
+
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'Bsamw_hpL4A',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': (event) => event.target.playVideo()
+        }
+    });
+}
+
+function loadIframe() {
     var tag = document.createElement('script');
 
     tag.src = "https://www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    var player;
-
-    function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-            height: '360',
-            width: '640',
-            videoId: 'Bsamw_hpL4A',
-            events: {
-                'onReady': onPlayerReady,
-            }
-        });
-    }
-
-    function onPlayerReady(event) {
-        event.target.playVideo();
-    }
-});
+}
