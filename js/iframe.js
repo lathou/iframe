d197e530648036d088f5147e163760cd4d00f618
lathou <tholfou@gmail.com>
2021-05-27 12:00:02 +0200

    add mute on iframe with autoplay

--- js/iframe.js
+++ js/iframe.js
@@ -4,7 +4,7 @@ const playBtnVimeo = document.querySelector('.js-play-vimeo');
 const wrapper = document.querySelector('.wrapper');
 
 playBtn.addEventListener('click', () => {
-    wrapper.innerHTML = "<iframe frameborder=\"0\" allowfullscreen=\"1\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" title=\"YouTube video player\" width=\"50%\" height=\"100%\" src=\"https://www.youtube.com/embed/Bsamw_hpL4A?autoplay=1&mute=0&controls=1&origin=https%3A%2F%2Frecette.devoirs.objectif-crpe.fr&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&enablejsapi=1&widgetid=1\" id=\"widget2\"></iframe>"
+    wrapper.innerHTML = "<iframe frameborder=\"0\" allowfullscreen=\"1\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" title=\"YouTube video player\" width=\"50%\" height=\"100%\" src=\"https://www.youtube.com/embed/Bsamw_hpL4A?autoplay=1&mute=0&controls=1&origin=https%3A%2F%2Frecette.devoirs.objectif-crpe.fr&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&enablejsapi=1&widgetid=1&mute=1\" id=\"widget2\"></iframe>"
 });
 
 playBtnYt.addEventListener('click', () => {
